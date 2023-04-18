8da0e29e1b7c23c5e5e001030f20cb30f031b6f3
Louis Ponet <louisponet@gmail.com>
2023-04-18 16:05:49 +0200

    added constructors for alpaca with max requests

--- pvm_cpp/brokers/alpaca.cpp
+++ pvm_cpp/brokers/alpaca.cpp
@@ -7,8 +7,10 @@ httplib::Headers headers(const std::string& alpaca_key_id, const std::string& al
   };
 
 }
+AlpacaBroker::AlpacaBroker(const std::string alpaca_key_id_, 
+                           const std::string alpaca_secret_,
+						   const int max_requests): cur_requests(0), max_requests(max_requests){
 
-AlpacaBroker::AlpacaBroker(const std::string alpaca_key_id_, const std::string alpaca_secret_){
 	httplib::Client client("paper-api.alpaca.markets");
 
 	auto resp = client.Get("/v2/account", ::headers(alpaca_key_id_, alpaca_secret_));
@@ -18,12 +20,22 @@ AlpacaBroker::AlpacaBroker(const std::string alpaca_key_id_, const std::string a
 
 	alpaca_key_id = alpaca_key_id_;
 	alpaca_secret = alpaca_secret_;
-};
+
+}
+
+AlpacaBroker::AlpacaBroker(const std::string alpaca_key_id_, 
+                           const std::string alpaca_secret_) : 
+	AlpacaBroker(alpaca_key_id_, alpaca_secret_, 200){}
 
 httplib::Headers AlpacaBroker::headers() {
 	return ::headers(alpaca_key_id, alpaca_secret);
 };
 
-std::vector<Bar> AlpacaBroker::get_bars(){
+std::vector<Bar> AlpacaBroker::get_bars(const std::string& ticker,
+                                        const std::string& start,
+                                        const std::string& stop,
+                                        const std::string& timeframe){
+
+
 
 }
--- pvm_cpp/brokers/alpaca.hpp
+++ pvm_cpp/brokers/alpaca.hpp
@@ -1,16 +1,24 @@
 #pragma once
 
 #include <string>
+#include <atomic>
 #include "httplib.h"
 #include "pvm_cpp/bar.hpp"
 
 class AlpacaBroker {
 	public:
-		AlpacaBroker(const std::string alpaca_key_id, const std::string alpaca_secret);
+		AlpacaBroker(const std::string alpaca_key_id, 
+		             const std::string alpaca_secret);
+		AlpacaBroker(const std::string alpaca_key_id, 
+		             const std::string alpaca_secret,
+					 const int max_requests);
 		AlpacaBroker() = delete;
 
 
-		std::vector<Bar> get_bars();
+		std::vector<Bar> get_bars(const std::string& ticker,
+                                  const std::string& start,
+                                  const std::string& stop,
+                                  const std::string& timeframe);
 	private:
 
 		httplib::Headers headers();
@@ -18,5 +26,6 @@ class AlpacaBroker {
 		std::string alpaca_key_id;
 		std::string alpaca_secret;
 
-
+		std::atomic<int> cur_requests;
+		const int max_requests; // per minute
 };
